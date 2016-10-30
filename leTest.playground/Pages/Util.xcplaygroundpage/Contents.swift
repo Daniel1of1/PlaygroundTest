bc3bd1daa0c30d20e441bdca2781f9586476aa77
Daniel Haight <daniel+code@haight.io>
2016-10-30 10:40:26 +0000

    asdfadsfaf

--- leTest.playground/Pages/Util.xcplaygroundpage/Contents.swift
+++ leTest.playground/Pages/Util.xcplaygroundpage/Contents.swift
@@ -1,20 +1,2 @@
-import class Foundation.NSCharacterSet
-
-public extension String {
-
-    var isEmptyOrWhitespace: Bool {
-        return characters.isEmpty ? true : trimmingCharacters(in: NSCharacterSet.whitespaces) == ""
-    }
-
-
-    var isNotEmptyOrWhitespace: Bool {
-        return !isEmptyOrWhitespace
-    }
-    
-}
 
 public let someConstString = "YOLO!!!"
-
-func yolo() {
-    print("asdfasd")
-}
